adfad5f12b811207b6c687d7e5cb4904abf69cc0
SANJIKS <sanzarmaratov588@gmail.com>
2024-02-26 17:45:38 +0600

    changed swagger

--- core/urls.py
+++ core/urls.py
@@ -24,9 +24,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Electronic Queue System",
+        title="FacePay Bank",
         default_version='v1',
-        description="API for RSK",
+        description="API for Hackaton",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
